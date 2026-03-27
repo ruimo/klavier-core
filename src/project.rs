1869ba81c8264b58be7195f5d8c713a95e8d4e12
CI <shanai@jp.ibm.com>
2026-03-28 08:30:00 +0900

    update libs

--- src/project.rs
+++ src/project.rs
@@ -1,7 +1,6 @@
 use std::rc::Rc;
 use std::slice;
 
-use error_stack::IntoReport;
 use klavier_helper::bag_store::{BagStore, BagStoreEvent};
 use klavier_helper::store::{Store, StoreEvent};
 use serde::{Serialize, Deserialize};
@@ -692,7 +691,7 @@ impl Project for SqliteUndoStore::<ProjectCmd, ProjectImpl, ProjectCmdErr> {
                     }
                 )
             } else {
-                Err(IntoReport::into_report(ProjectCmdErr::NoOp))
+                Err(ProjectCmdErr::NoOp)
             }
         }));
         
